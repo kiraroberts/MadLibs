339af98a39416297b03569ee1e51cc4f9fbc279a
Jennifer Batara and Kira Roberts <rosehill1788@protonmail.com>
2019-10-07 16:20:35 -0700

    add another madlib, two submit buttons

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -8,8 +8,39 @@ namespace MadLibs.Controllers
         [Route("/")]
         public ActionResult Form() {return View(); } 
 
-        [Route("/output")]
-        public ActionResult Output(string noun, string adjective, string verb, string adverb) 
+         [Route("/output")]
+        public ActionResult Puppy(string noun, string adjective, string verb, string adverb, string prompt) 
+        {
+            WordHolder words = new WordHolder();
+            words.Noun = noun;
+            words.Adjective = adjective;
+            words.Verb = verb;
+            words.Adverb = adverb;
+            // words.Prompt = prompt;
+            if (prompt == "puppy")
+            {
+                return View("~/Views/Home/Puppy.cshtml", words);
+            }
+            else if (prompt == "RV")
+            {
+                return View("~/Views/Home/RV.cshtml", words);
+            }
+            return View(words);
+        }
+
+
+        [Route("/puppy")]
+        public ActionResult Puppy(string noun, string adjective, string verb, string adverb) 
+        {
+            WordHolder words = new WordHolder();
+            words.Noun = noun;
+            words.Adjective = adjective;
+            words.Verb = verb;
+            words.Adverb = adverb;
+            return View(words);
+        }
+        [Route("/rv")]
+        public ActionResult RV(string noun, string adjective, string verb, string adverb) 
         {
             WordHolder words = new WordHolder();
             words.Noun = noun;
--- MadLibs/Models/WordHolder.cs
+++ MadLibs/Models/WordHolder.cs
@@ -6,6 +6,8 @@ namespace MadLibs.Models
         public string Noun {get; set;}
         public string Adverb {get;set;}
         public string Adjective {get;set;}
+
+        // public string Prompt{get; set;}
         // public WorldHolder(string verb, string noun, string adverb, string adjective)
         // {
         //     Verb = verb;
